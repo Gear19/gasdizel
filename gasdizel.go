3192d67fb3f63336c2439fabc8c43cd7fad6aed9
Gear19 <www.roma.ibr@gmail.com>
2023-06-06 19:19:16 +0300

    Update gasdizel

--- gasdizel.go
+++ gasdizel.go
@@ -4,9 +4,9 @@ type Liters float64
 type Gallons float64
 
 func (m Liters) ToGallons(s Gallons) Liters {
-	return Liters(s)
+	return Liters(s * 3.785)
 }
 
 func (m Gallons) ToLiters(s Liters) Gallons {
-	return Gallons(s)
+	return Gallons(s * 0.264)
 }
